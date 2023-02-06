cad1f5e41ec4caaf74ca3cace0cd4029a2cf2749
BlackStan <116716845+BlackStan@users.noreply.github.com>
2023-02-06 17:18:45 +0100

    v3

--- API.js
+++ API.js
@@ -13,14 +13,20 @@ const pool = mysql.createPool({
 const db = pool.promise();
 var haustiere = []
 
-db.execute('Select * From Haustiere').then(result => {
-    console.log(result[0].length)                              //Für mich um ergebnise in der Konsole zu
 
-    for(let i = 0; i < result[0].length; i++){
-        haustiere[i] = result[0][i];
-        console.log(haustiere[i]);
-    }
-});
+
+function datenBankLesen(){
+    db.execute('Select * From Haustiere').then(result => {
+        console.log(result[0].length)                              //Für mich um ergebnise in der Konsole zu
+    
+        for(let i = 0; i < result[0].length; i++){
+            haustiere[i] = result[0][i];
+            console.log(haustiere[i]);
+        }
+    });
+}
+
+datenBankLesen();
 
 const app = express();
 app.use(cors())
@@ -30,6 +36,8 @@ app.get('/list',(req, res) =>{
    res.end();
 })
 
+
+
 app.get('/get/:id', (req, res) => {
     var ItemId = parseInt(req.params.id);
     const hausTier = haustiere.filter(item =>{
@@ -38,7 +46,34 @@ app.get('/get/:id', (req, res) => {
 
     res.json(hausTier)
     res.end();
-})
+});
+
+
+
+app.post('/eintrag', (req, res) => {
+
+    const eingabe = []; //Array um die Chunks zu empfangen
+    
+    req.on('data', (chunk) =>{      //Die abgeschickten werden als hexadezimal Stück für Stück geladen
+        eingabe.push(chunk);        //und in die eingabe als Array gelegt
+        console.log("Request Succesed")
+    });
+
+    req.on('end', () =>{            //Wenn die Daten vollständig empfangen wurden
+        const parsed = Buffer.concat(eingabe).toString();       //wird das Array zusammen geführt und in ein String umgewandelt
+        const splitParsed = parsed.split('&');                  //Array wird bei & gesplitet
+        const tierName = splitParsed[0].split('=')[1];          //Erneut bei = gesplittet und die Werte laden
+        const tierRasse = splitParsed[1].split('=')[1];
+
+        res.statusCode = 302;           
+        res.writeHead(301,{
+            Location: 'http://127.0.0.1:5500/Client.html'
+        }).end()
+
+        db.execute('INSERT INTO Haustiere (_name, _rasse) Values (?, ?)', [tierName, tierRasse]); //Der datenbank hinzufügen
+        datenBankLesen();
+    })
+});
 
 
 const server = http.createServer(app)
